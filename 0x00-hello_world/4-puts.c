70df2cdd45a4b41305e45ffcb8860d97c5a6306d
dmac24 <dinitayala@gmail.com>
2021-02-04 18:40:14 +0000

    fixed check of code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle.
+ * main - Prints "Programming is like building a multilingual puzzle.
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-  return (0);
+return (0);
 }
