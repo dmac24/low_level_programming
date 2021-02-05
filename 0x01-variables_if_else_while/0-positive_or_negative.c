28b4d661882515516651bb355fc6ce2d9a1edb6f
dmac24 <dinitayala@gmail.com>
2021-02-05 20:48:36 +0000

    fix error = in function

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,9 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
  * main - conditional value of 0
- *
- * Description: if the number is greater than 0: is positive, if the  * number is 0: is zero, if the number is less than 0: is negative.
  *
  * Return: Always 0.
  */
@@ -13,15 +11,21 @@
 int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
 if (n > 0)
-printf("%d is positive\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
+{
+printf("%i is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%i is zero\n", n);
+}
 else
-printf("%d is zero\n", n);
+{
+printf("%i is negative\n", n);
+}
 
 return (0);
 }
