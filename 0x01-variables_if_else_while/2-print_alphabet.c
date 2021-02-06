0f2626d85676e4f3bb5066d386697287eefaa020
dmac24 <dinitayala@gmail.com>
2021-02-06 00:45:07 +0000

    alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Read the last digit
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char c = 'a';
+while (c < '{')
+{
+putchar(c);
+c++;
+}
+
+return (0);
+}
