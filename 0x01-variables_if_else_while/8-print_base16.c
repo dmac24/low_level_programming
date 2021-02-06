04eb01ac24db7775a9ff3f9c010b350877c4dfc5
dmac24 <dinitayala@gmail.com>
2021-02-06 02:58:10 +0000

    numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Read the last digit
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int c = 48;
+int d = 97;
+
+while (c < 58)
+{
+putchar(c);
+c++;
+}
+while (d < 103)
+{
+putchar(d);
+d++;
+}
+
+putchar('\n');
+
+return (0);
+}
