ec9a77852ae09b347fe11a93f64f6764e42dd914
dmac24 <56034849+dmac24@users.noreply.github.com>
2021-02-09 18:55:26 -0500

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+int c = 97;
+while (c < 122)
+{
+_putchar(c++);
+}
+
+return (0);
