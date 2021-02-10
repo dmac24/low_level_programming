ca2f479b6965d7529beebad80318c63c0db47174
dmac24 <dinitayala@gmail.com>
2021-02-10 00:39:50 +0000

    alphabet line x 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,19 @@
 #include "holberton.h"
 
 /**
- * print alphabet - print alphabet
+ * print alphabet - print alphabet x10
  *
  * Return: Always 0.
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 int c = 'a';
+int n = '0';
+while (n < 10)
+{
+_putchar(10);
+}  
 while (c <= 'z')
 {
 _putchar(c++);
