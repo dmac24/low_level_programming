ba85f52719efc5a1112f536765fd7d437801e298
dmac24 <dinitayala@gmail.com>
2021-02-11 19:56:47 +0000

    fix code to print 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 
 /**
- * print alphabet - print alphabet x10
+ * print_alphabet_x10 - print alphabet x10
  *
  * Return: Always 0.
  */
 
 void print_alphabet_x10(void)
 {
-int n = '0';
+int s = 0;
 int c = 'a';
-while (n < 10)
+
+while (s++ <= 9)
 {
-while (c <= 'z')
+for (c = 'a'; c <= 'z'; c++)
 {
-_putchar(c++);
+_putchar(c);
 }
-n++;
 _putchar('\n');
 }
 }
