8b83e16b2ca8b825f112e5fb131311acaef3ea9c
dmac24 <dinitayala@gmail.com>
2021-02-12 03:09:26 +0000

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints diagonal n times
+ *
+ * @n: * times
+ *
+ * Return: void.
+ */
+
+void print_diagonal(int n)
+{
+int a, b;
+
+if (n <= 0)
+_putchar('\n');
+for (a = 0; a < n; a++)
+{
+for (b = 0; b < a; b++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
