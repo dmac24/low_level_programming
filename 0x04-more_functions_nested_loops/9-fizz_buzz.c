a2886147b90a14c89c4e3076a7f10898e304e65c
dmac24 <dinitayala@gmail.com>
2021-02-12 03:52:32 +0000

    fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Alays o
+ */
+
+int main(void)
+{
+int i;
+
+i = 1;
+while (i <= 100)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
+if (i == 100)
+break;
+putchar(' ');
+i++;
+}
+putchar('\n');
+return (0);
+}
