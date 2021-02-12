8b5d27e184e620587f49c951227c19d449304dc7
dmac24 <dinitayala@gmail.com>
2021-02-12 00:07:46 +0000

    don't print 2 numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: void.
+ */
+
+void print_most_numbers(void)
+{
+int c = '0';
+while (c <= '9')
+{
+if ((c == '2') || (c == '4'))
+{
+c++;
+}
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
