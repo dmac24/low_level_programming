f38a5e2665b35d1010fd85bf9e497845e1ab14b6
dmac24 <dinitayala@gmail.com>
2021-02-17 02:30:23 +0000

    length of a string and mains of prototypes

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - return the lenght of a string
+ *
+ * @s: string
+ *
+ * Return: legth of s
+ */
+
+int _strlen(char *s)
+{
+
+int l = 0;
+
+while (*s++)
+l++;
+
+return (l);
+}
