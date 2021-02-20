1404564f732ec3da0cd99434af9f7916596c7302
dmac24 <dinitayala@gmail.com>
2021-02-20 02:18:25 +0000

    function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strncat - add src to the dest string
+ *
+ * @dest: string add to src
+ * @src: string add to dest
+ * @n: numbers of bytes
+ *
+ * Return: dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+int i = 0;
+int j = 0;
+while (dest[i] != '\0')
+i++;
+while (src[j] != '\0' && j != n)
+{
+dest[i++] = src[j++];
+}
+return (dest);
+}
