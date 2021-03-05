b5b9c196fc5133915803588fd855c2e535261583
dmac24 <dinitayala@gmail.com>
2021-03-05 00:44:59 +0000

    fixed betty style

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,13 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
+printf("Infinite loop avoided! \\o/\n");
 /**
- * while (i < 10)
+ * while (i < 10) - loop
  * {
  * putchar(i);
  * }
  */
-printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
