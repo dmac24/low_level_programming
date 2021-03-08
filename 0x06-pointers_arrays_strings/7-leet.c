cee5b21614921e2fa984c80ab67a28fefadc9d40
dmac24 <dinitayala@gmail.com>
2021-03-08 16:54:52 +0000

    Write a function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * leet - function that encodes a string into 1337.
+ *
+ * @s: string
+ *
+ * Return: String.
+ */
+
+char *leet(char *s)
+
+{
+
+int i, j;
+char original[] = "aAeEoOtTlL";
+char change[] = "4433007711";
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; s[j] != '\0'; j++)
+{
+if (s[i] == original[j])
+{
+s[i] = change[j];
+}
+}
+}
+return (s);
+}
