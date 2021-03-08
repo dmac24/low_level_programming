b26a5a69e8e5657ef65e53071ea44cd4d635f3ee
dmac24 <dinitayala@gmail.com>
2021-03-08 17:12:46 +0000

    Write a function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * rot13 - funtion that encodes a string using rot13
+ *
+ * @s: string
+ *
+ * Return: String modified.
+ */
+
+char *rot13(char *s)
+
+{
+
+int i, j;
+char original[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char change[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (s[i] == original[j])
+{
+s[i] = change[j];
+}
+}
+}
+return (s);
+}
