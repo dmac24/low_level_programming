ad9c7840d1718da8405a12be37ccd083cb9cec0d
dmac24 <dinitayala@gmail.com>
2021-03-09 03:29:02 +0000

    output corrected

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,22 +16,22 @@ void print_dog(struct dog *d)
 if (d != NULL)
 {
 
-if (d->name != NULL)
-printf("Name: %s\n", d->name);
-else
+if (d->name == NULL)
 printf("Name: (nil)\n");
+else
+printf("Name: %s\n", d->name);
 
-if (d->age != 0)
+if (d->age > 0)
 printf("Age: %f\n", d->age);
 else
 printf("Age: (nil)\n");
 
-if (d->owner != NULL)
-printf("Owner: %s\n", d->owner);
-else
+if (d->owner == NULL)
 printf("Owner: (nil)\n");
-
+else
+printf("Owner: %s\n", d->owner);
 }
+
 else
 return;
 }
