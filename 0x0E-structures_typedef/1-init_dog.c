d9913991a941ca9f514819896c622ebac8ee730f
dmac24 <dinitayala@gmail.com>
2021-03-09 01:08:34 +0000

    Initial Commit, Empty files. Readme and dog.hcreation

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Recursion that prints a string in reverse.
+ *
+ * @s: String.
+ *
+ * Return: Void.
+ */
+
+void _print_rev_recursion(char *s)
+
+{
+
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - information about dogs
+ * @name: Dog's name.
+ * @age: Dog's age.
+ * @owner: Dog's owner.
+ *
+ * Description: Define a new type struct dog with some elements.
+ */
+
+struct dog
+{
+char *name;
+float *age;
+char *owner;
+};
+
+#endif
