d6b108649e3817d327391525468115f06a553c4b
dmac24 <dinitayala@gmail.com>
2021-03-10 23:49:25 +0000

    Initial commit. Empty Files

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strchr - function that locates a character in a string.
+ *
+ * @s: string
+ * @c: Character
+ *
+ * Return: Legth Of S
+ */
+
+char *_strchr(char *s, char c)
+{
+int i;
+
+for (i = 0; s[i]; i++)
+{
+if (s[i] == c)
+return (s + i);
+}
+if (s[i] == c)
+return (s + i);
+return (0);
+}
