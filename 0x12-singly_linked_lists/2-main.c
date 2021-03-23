636c633f6e072f9b75cd91f7448123d4ea12b2f4
dmac24 <dinitayala@gmail.com>
2021-03-23 23:44:28 +0000

    function that adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/2-main.c
+++ 0x12-singly_linked_lists/2-main.c
@@ -1,5 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-#include "dog.h"
+#include "lists.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -8,11 +10,37 @@
  */
 int main(void)
 {
-  struct dog my_dog;
+  list_t *head;
 
-  my_dog.name = "Django";
-  my_dog.age = 3.5;
-  my_dog.owner = "Bob";
-  print_dog(&my_dog);
+  head = NULL;
+  add_node(&head, "Alexandro");
+  add_node(&head, "Asaia");
+  add_node(&head, "Augustin");
+  add_node(&head, "Bennett");
+  add_node(&head, "Bilal");
+  add_node(&head, "Chandler");
+  add_node(&head, "Damian");
+  add_node(&head, "Daniel");
+  add_node(&head, "Dora");
+  add_node(&head, "Electra");
+  add_node(&head, "Gloria");
+  add_node(&head, "Joe");
+  add_node(&head, "John");
+  add_node(&head, "John");
+  add_node(&head, "Josquin");
+  add_node(&head, "Kris");
+  add_node(&head, "Marine");
+  add_node(&head, "Mason");
+  add_node(&head, "Praylin");
+  add_node(&head, "Rick");
+  add_node(&head, "Rick");
+  add_node(&head, "Rona");
+  add_node(&head, "Siphan");
+  add_node(&head, "Sravanthi");
+  add_node(&head, "Steven");
+  add_node(&head, "Tasneem");
+  add_node(&head, "William");
+  add_node(&head, "Zee");
+  print_list(head);
   return (0);
 }
