93682fff6373d57f649fca57de821cc333d98e22
dmac24 <dinitayala@gmail.com>
2021-03-24 00:09:01 +0000

    function that adds a new node at the end of a list_t list.

--- 0x12-singly_linked_lists/3-main.c
+++ 0x12-singly_linked_lists/3-main.c
@@ -1,5 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-#include "dog.h"
+#include "lists.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -8,11 +10,37 @@
  */
 int main(void)
 {
-  dog_t my_dog;
+  list_t *head;
 
-  my_dog.name = "Django";
-  my_dog.age = 3.5;
-  my_dog.owner = "Bob";
-  printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+  head = NULL;
+  add_node_end(&head, "Anne");
+  add_node_end(&head, "Colton");
+  add_node_end(&head, "Corbin");
+  add_node_end(&head, "Daniel");
+  add_node_end(&head, "Danton");
+  add_node_end(&head, "David");
+  add_node_end(&head, "Gary");
+  add_node_end(&head, "Holden");
+  add_node_end(&head, "Ian");
+  add_node_end(&head, "Ian");
+  add_node_end(&head, "Jay");
+  add_node_end(&head, "Jennie");
+  add_node_end(&head, "Jimmy");
+  add_node_end(&head, "Justin");
+  add_node_end(&head, "Kalson");
+  add_node_end(&head, "Kina");
+  add_node_end(&head, "Matthew");
+  add_node_end(&head, "Max");
+  add_node_end(&head, "Michael");
+  add_node_end(&head, "Ntuj");
+  add_node_end(&head, "Philip");
+  add_node_end(&head, "Richard");
+  add_node_end(&head, "Samantha");
+  add_node_end(&head, "Stuart");
+  add_node_end(&head, "Swati");
+  add_node_end(&head, "Timothy");
+  add_node_end(&head, "Victor");
+  add_node_end(&head, "Walton");
+  print_list(head);
   return (0);
 }
