0aa8ba539184554a1b4b5ae4d511ca91584a93e0
dmac24 <dinitayala@gmail.com>
2021-04-05 18:00:48 +0000

    betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,23 +19,24 @@
 int create_file(const char *filename, char *text_content)
 
 {
-  int fd, nwrite;
-  int i = 0;
+int fd, nwrite;
+int i = 0;
 
-  if (filename == NULL)
-    return (-1);
+if (filename == NULL)
+return (-1);
 
-  if (text_content == NULL)
-    text_content = "";
-  fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+if (text_content == NULL)
+text_content = "";
+fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 
-  if (fd == -1)
-    return (-1);
+if (fd == -1)
+return (-1);
 
-  while (text_content[i] != '\0')
-    i++;
-  nwrite = write(fd, text_content, i);
-  if (nwrite == -1)
-    return (-1);
-  return (1);
+while (text_content[i] != '\0')
+i++;
+nwrite = write(fd, text_content, i);
+if (nwrite == -1)
+return (-1);
+return (1);
+
 }
