88bfb96fe2b714cca28c53ae04df01728099ba01
Diana Ayala <56034849+dmac24@users.noreply.github.com>
2021-11-02 17:11:13 -0500

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,21 +2,78 @@
 #include "dog.h"
 
 /**
+ * _strdup - create a new array containing a copy of the given string
+ * @str: a pointer to the string to copy
+ *
+ * Return: NULL if str is NULL or if memory allocation fails,
+ * otherwise a return a pointer to the new copy
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int size = 0;
+
+if (str)
+	{
+		while (str[size++])
+			;
+
+		dup = malloc(sizeof(char) * size);
+		if (dup)
+		{
+			while (size--)
+				dup[size] = str[size];
+
+			return (dup);
+		}
+	}
+	return (NULL);
+}
+
+
+/**
  * new_dog - creates a new dog
  * @name: Dog's name
  * @age: Dog's age
  * @owner: Dog's owner
  *
- * Return: pointer to new dog
+ * Return: null or pointer to new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
 
-{
-if (d)
-{
-d->name = name;
-d->age = age;
-d->owner = owner;
-}
+	d = malloc(sizeof(dog_t));
+	if (!d)
+		return (NULL);
+
+	if (name)
+	{
+		d->name = _strdup(name);
+		if (!(d->name))
+		{
+			free(d);
+			return (NULL);
+		}
+	}
+	else
+		d->name = NULL;
+
+	d->age = age;
+
+	if (owner)
+	{
+		d->owner = _strdup(owner);
+		if (!(d->owner))
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
+	}
+	else
+		d->owner = NULL;
+
+return (d);
 }
