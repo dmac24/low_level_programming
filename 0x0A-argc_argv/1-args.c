a3437ef81963c70bde525078f5ba64ed63e4087f
dmac24 <dinitayala@gmail.com>
2021-12-01 03:52:44 +0000

    print a number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: int
+ * @argv: char
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return(0);
+}
