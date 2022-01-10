93a1124eff25d0fcef48fdfaec101052efb95831
dmac24 <dinitayala@gmail.com>
2022-01-10 19:14:03 +0000

    fix null in head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,6 +14,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
+	if (head == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
