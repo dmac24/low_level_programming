9271bbc834c43e2cbf0c932901ffb724992c71c3
dmac24 <dinitayala@gmail.com>
2022-01-10 22:19:06 +0000

    fix error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,45 +10,47 @@
  * @n: for new node
  * Return: address of new node or NULL
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current;
 	dlistint_t *new;
 	unsigned int i;
 
-	current = *h;
-
-	if (h == NULL)
-		return (NULL);
-	if (idx != 0)
-	{
-		for (i = 0; i < idx - 1 && current != NULL; i++)
-			current = current->next;
-	}
-	if (idx > i + 1)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+
+	if (!new)
 		return (NULL);
 
+	current = *h;
 	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
 	if (idx == 0)
 	{
-		new->next = *h;
-		new->prev = NULL;
+		if (*h)
+		{
+			new->next = *h;
+			(*h)->prev = new;
+		}
 		*h = new;
-		return (new);
 	}
-	if (idx == i)
+	else
 	{
+		for (i = 0; i < idx - 1; i++)
+		{
+			if (!current)
+				return (NULL);
+			current = current->next;
+		}
+
+		if (current->next)
+		{
+			new->next = current->next;
+			current->next->prev = new;
+		}
 		new->prev = current;
-		new->next = NULL;
 		current->next = new;
-		return (new);
 	}
-	new->prev = current;
-	new->next = current->next;
-	current->next = new;
 	return (new);
 }
