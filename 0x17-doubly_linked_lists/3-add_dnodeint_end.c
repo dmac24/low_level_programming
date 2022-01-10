c498ea76089fe158e268ef40f3374ce14b54f365
dmac24 <dinitayala@gmail.com>
2022-01-10 21:57:23 +0000

    betty style

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -30,10 +30,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	else
 	{
 		while (current->next != NULL)
-	       		current = current->next;
+			current = current->next;
 
 		current->next = new;
-		new-> prev = current;
+		new->prev = current;
 	}
 
 	return (new);
