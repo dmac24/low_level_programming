c38d8c15ddda979c91a03f9f9f8ff7c717965a33
dmac24 <dinitayala@gmail.com>
2022-01-10 21:54:03 +0000

    fix error

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -30,11 +30,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	else
 	{
 		while (current->next != NULL)
-		{
-			new->prev = current;
-			current = current->next;
-		}
+	       		current = current->next;
+
 		current->next = new;
+		new-> prev = current;
 	}
 
 	return (new);
